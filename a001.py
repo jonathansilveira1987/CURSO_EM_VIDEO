816dd3e582b3e9a44c9307d9ab6d5d6948adfef3
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-17 00:31:28 -0300

    Exercício

--- a001.py
+++ a001.py
@@ -1,3 +1,7 @@
 l = [1, 2, 3]
 l.extend((3, 4, 5))
 print(l)
+
+
+p = (2, 3)
+print(2 * p)
---
+++ cores_python.py
@@ -0,0 +1,20 @@
+# Cores no Python
+
+# Style                  Text               Back
+#   0 - nenhum            30 - branco        40 - branco
+#   1 - Negrito           31 - vermelho      41 - vermelho
+#   4 - Sublinhado        32 - verde         42 - verde
+#   7 - Negativo          33 - amarelo       43 - amarelo
+#                         34 - azul          44 - azul
+#                         35 - roxo          45 - roxo
+#                         36 - celeste       46 - celeste   
+#                         37 - cinza         47 - cinza
+
+
+print("\n\033[0;31m Olá Mundo!\n")
+print("\033[0;32m Olá Mundo!\n")
+print("\033[0;33m Olá Mundo!\n")
+print("\033[0;34m Olá Mundo!\n")
+print("\033[0;35m Olá Mundo!\n")
+print("\033[0;36m Olá Mundo!\n")
+print("\033[0;37m Olá Mundo!\n")
