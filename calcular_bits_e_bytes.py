8c8307300034eafd5c3cea234bd6b0df860fd833
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-18 00:46:28 -0300

    Exercício

--- calcular_bits_e_bytes.py
+++ calcular_bits_e_bytes.py
@@ -24,6 +24,8 @@ if unidade == 2:
     bit = (2 ** 10) * 8
     print("\n-> 1 Kilobyte possui {0:,} Bytes.".format(kilobyte).replace(",", "."))
     print("-> 1 Kilobyte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("8 mil .192 bits.\n")
 # MegaByte.
 if unidade == 3:
     megabyte = 1 * 1024
@@ -32,6 +34,8 @@ if unidade == 3:
     print("\n-> 1 Megabyte possui {0:,} Kilobytes.".format(megabyte).replace(",", "."))
     print("-> 1 Megabyte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 Megabyte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("8 milhões .388 mil .608 bits.\n")
 # GigaByte.
 if unidade == 4:
     gigabyte = 1 * 1024
@@ -40,6 +44,8 @@ if unidade == 4:
     print("\n-> 1 GigaByte possui {0:,} MegaBytes.".format(gigabyte).replace(",", "."))
     print("-> 1 GigaByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 GigaByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("8 bilhões .589 milhões .934 mil .592 bits.\n")
 # TeraByte.
 if unidade == 5:
     terabyte = 1 * 1024
@@ -48,6 +54,8 @@ if unidade == 5:
     print("\n-> 1 TeraByte possui {0:,} GigaBytes.".format(terabyte).replace(",", "."))
     print("-> 1 TeraByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 TeraByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("8 trilhões .796 bilhões .093 milhões .022 mil .208 bits.\n")
 # PetaByte.
 if unidade == 6:
     petabyte = 1 * 1024
@@ -56,6 +64,8 @@ if unidade == 6:
     print("\n-> 1 PetaByte possui {0:,} TeraBytes.".format(petabyte).replace(",", "."))
     print("-> 1 PetaByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 PetaByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("9 quatrilhões .007 trilhões .199 bilhões .254 milhões .740 mil .992 bits.\n")
 # 7. ExaByte
 if unidade == 7:
     exabyte = 1 * 1024
@@ -64,6 +74,8 @@ if unidade == 7:
     print("\n-> 1 ExaByte possui {0:,} PetaBytes.".format(exabyte).replace(",", "."))
     print("-> 1 ExaByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 ExaByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("9 quintilhões .223 quatrilhões .372 trilhões .036 bilhões .854 milhões .775 mil .808 bits.\n")
 # 8. ZettaByte
 if unidade == 8:
     zettabyte = 1 * 1024
@@ -72,6 +84,8 @@ if unidade == 8:
     print("\n-> 1 ZettaByte possui {0:,} ExaBytes.".format(zettabyte).replace(",", "."))
     print("-> 1 ZettaByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 ZettaByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("9 sextilhões .444 quintilhões .732 quatrilhões .965 trilhões .739 bilhões .290 milhões .427 mil .392 bits.\n")
 # 9. YottaByte
 if unidade == 9:
     yottabyte = 1 * 1024
@@ -80,6 +94,8 @@ if unidade == 9:
     print("\n-> 1 YottaByte possui {0:,} ZettaBytes.".format(yottabyte).replace(",", "."))
     print("-> 1 YottaByte possui {0:,} Bytes.".format(byte).replace(",", "."))
     print("-> 1 YottaByte possui {0:,} Bits.\n".format(bit).replace(",", "."))
+    print("Abaixo segue número lido por extenso...")
+    print("9 septilhões .671 sextilhões .406 quintilhões .556 quatrilhões .917 trilhões .033 bilhões .397 milhôes .649 mil .408 bits.\n")
 
 
 
@@ -107,7 +123,3 @@ if unidade == 9:
 
 # Isso só vale para versão do python 2.7 + .
 # A partir dessa resposta do SOEN, é possível fazer para versões mais antigas utilizando o locale.format()
-
-
-
-
