b233d815ef4a74df6590d8fd6d2ee746d027002c
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-08-24 00:34:34 -0300

    Exercício

--- ex056.py
+++ ex056.py
@@ -11,21 +11,19 @@ for p in range(1, 5):
     nome = str(input("Nome: ")).strip()
     idade = int(input("Idade: "))
     sexo = str(input("Sexo [M/F]: ")).strip()
-    somaidade += idade
+    somaidade = somaidade + idade
 
     if p == 1 and sexo in "Mn":
         maioridadehomem = idade
-        nomevelho = idade
-
+        nomevelho = nome
     if sexo in "Mn" and idade > maioridadehomem:
         maioridadehomem = idade
         nomevelho = nome
-
     if sexo in "Ff" and idade > 20:
         totmulher20 += 1
 
 mediaidade = somaidade / 4
 
 print("A média de idade do grupo é de {} anos.".format(mediaidade))
-print("O homem mais velho tem {} anos e se chama {}.".format(maioridadehomem, nomevelho))
+print(f"O homem mais velho tem {maioridadehomem} anos e se chama {nomevelho}.")
 print("Ao todo são {} mulheres com menos de 20 anos.".format(totmulher20))
--- ex074.py
+++ ex074.py
@@ -9,4 +9,4 @@ print(f"Os valores sorteados foram: ", end="")
 for n in numeros:
     print(f"{n} ", end="")
 print(f"\nO maior valor sorteado foi {max(numeros)}.")
-print(f"O mnenor valor sorteado foi {min(numeros)}.")
+print(f"O menor valor sorteado foi {min(numeros)}.")
