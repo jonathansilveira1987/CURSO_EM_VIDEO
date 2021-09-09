06827deeef29ce26e9b5931c49070a9f2eac7ce3
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-09-09 14:52:56 -0300

    Exercício

---
+++ ex095.py
@@ -0,0 +1,55 @@
+# Aprimorando os Dicionários.
+
+time = list()
+jogador = dict()
+partidas = list()
+
+while True:
+    jogador.clear()
+    jogador['Nome'] = str(input('Nome do Jogador: '))
+    tot = int(input(f'Quantas partidas {jogador["Nome"]} jogou? '))
+    partidas.clear()
+    for c in range(0, tot):
+        partidas.append(int(input(f' Quantos gols na partida {c+1}? ')))
+    jogador['gols'] = partidas[:]
+    jogador['total'] = sum(partidas)
+    time.append(jogador.copy())
+    while True:
+        resp = str(input('Quer continuar [S/N]? ')).upper()[0]
+        if resp in "SN":
+            break
+        print('ERRO! Responda apenas S ou N')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print('cod', end='')
+for i in jogador.keys():
+    print(f'{i:<15}', end='')
+print()
+print('-=' * 40)
+for k, v in enumerate(time):
+    print(f'{k:>3} ', end='')
+    for d in v.values():
+        print(f'{str(d):<15}', end='')
+    print()
+print('-=' * 40)
+
+
+
+
+# print('-=' * 30)
+# print(f'O jogador {jogador["Nome"]} jogou {len(jogador["gols"])} partidas.')
+# for i, v in enumerate(jogador['gols']):
+#     print(f'=> Na partida {i+1}, fez {v} gols.')
+# print(f'Foi um total de {jogador["total"]} gols.')
+
+
+
+
+
+
+
+
+
+
+
