2e3212171af87d7190dbab7580388dd12ba6cedd
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-10-17 23:55:17 -0300

    Exercício

--- a002.py
+++ a002.py
@@ -1,6 +1,11 @@
 from speedtest import Speedtest
 
 st = Speedtest()
+st.get_servers()
+best = st.get_best_server()
+ping_result = st.results.ping
 
-print('Sua conexão de download é: ', st.download())
-print('Sua conexão de upload é: ', st.upload())
+print(f"\nEncontrado servidor: {best['host']} localizado em {best['country']}.\n")
+print('Sua velocidade de Download é de {:.2f} Mbit/s.'.format(st.download() / 1024 / 1024))
+print('Sua velocidade de Upload é de {:.2f} Mbit/s.'.format(st.upload() / 1024 / 1024))
+print('Seu PING atual é de {:.2f} ms.\n'.format(ping_result))
