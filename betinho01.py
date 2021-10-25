05f197f708ef8ec6e5a2939f121f2006197b59fb
jonathansilveira1987 <jonathan.silveira@outlook.com>
2021-10-25 15:08:41 -0300

    Exercício

--- betinho01.py
+++ betinho01.py
@@ -3,11 +3,11 @@ idade = int(input("Digite a idade do aluno(a): "))
 
 if idade < 1:
     print("Digite uma idade válida.")
-if idade > 1 or idade <= 5:
+if idade >= 1 and idade <= 5:
     print(f"O aluno(a) {aluno} tem {idade} anos e está na Educação Infantil.".format(aluno, idade))
-elif idade >= 6 or idade <= 10:
+elif idade >= 6 and idade <= 10:
     print(f"O aluno(a) {aluno} tem {idade} anos e está no Ensino Fundamental I.".format(aluno, idade))
-elif idade >= 11 or idade <= 14:
+elif idade >= 11 and idade <= 14:
     print(f"O aluno(a) {aluno} tem {idade} anos e está no Ensino Fundamental II.".format(aluno, idade))
 else:
     print(f"O aluno(a) {aluno} tem {idade} anos e está no Ensino Médio.".format(aluno, idade))
